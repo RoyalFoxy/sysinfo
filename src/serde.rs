35d2bd0fb6c47d5d8e6833578a61dae5b0020fcf
RoyalFoxy <sepp.toni.mo@gmail.com>
2026-03-10 23:20:16 +0900

    Remove not needed fields to reduce the amount of data

--- src/serde.rs
+++ src/serde.rs
@@ -79,27 +79,18 @@ impl Serialize for crate::Process {
         S: Serializer,
     {
         // `19` corresponds to the (maximum) number of fields.
-        let mut state = serializer.serialize_struct("Process", 19)?;
+        let mut state = serializer.serialize_struct("Process", 10)?;
 
         state.serialize_field("name", &self.name().to_string_lossy())?;
-        state.serialize_field("cmd", &self.cmd())?;
         state.serialize_field("exe", &self.exe())?;
         state.serialize_field("pid", &self.pid().as_u32())?;
-        state.serialize_field("environ", &self.environ())?;
-        state.serialize_field("cwd", &self.cwd())?;
-        state.serialize_field("root", &self.root())?;
         state.serialize_field("memory", &self.memory())?;
-        state.serialize_field("virtual_memory", &self.virtual_memory())?;
-        state.serialize_field("parent", &self.parent())?;
         state.serialize_field("status", &self.status())?;
         state.serialize_field("start_time", &self.start_time())?;
         state.serialize_field("run_time", &self.run_time())?;
         state.serialize_field("cpu_usage", &self.cpu_usage())?;
-        state.serialize_field("accumulated_cpu_time", &self.accumulated_cpu_time())?;
         state.serialize_field("disk_usage", &self.disk_usage())?;
         state.serialize_field("user_id", &self.user_id())?;
-        state.serialize_field("group_id", &self.group_id())?;
-        state.serialize_field("session_id", &self.session_id())?;
 
         state.end()
     }
